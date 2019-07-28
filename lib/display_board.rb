5f23598e8313329ac325004907fc9b517af2b127
Brad Farberman <bfarberman@gmail.com>
2019-07-28 18:04:33 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,8 @@
 def display_board(board)
-  puts " #{board"
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "___________"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "___________"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  puts " "
+end
